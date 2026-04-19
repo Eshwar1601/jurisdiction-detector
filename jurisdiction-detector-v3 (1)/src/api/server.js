4c19737a53b56e496f707342e54fb430a50c78b2
Eswar Vankayalapati <eshwartejuvankayalapati1@gmail.com>
2026-04-19 20:15:25 +0530

    Update server.js

--- jurisdiction-detector-v3 (1)/src/api/server.js
+++ jurisdiction-detector-v3 (1)/src/api/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const path    = require("path");
+// Allow all origins (CORS fix)
+function setCORS(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') { res.sendStatus(200); return; }
+  next();
+}
 const { detectJurisdiction, detectBatch } = require("../orchestrator/jurisdictionOrchestrator");
 const { getStateConfig, getLocalTaxStates, getNoTaxStates } = require("../taxFilter/taxFilter");
 
 const app = express();
 app.use(express.json());
+app.use(setCORS);
 
 // Serve the frontend
 app.use(express.static(path.join(__dirname, "../../public")));
